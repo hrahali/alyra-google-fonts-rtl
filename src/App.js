dc568b331a0af45e56386588e16b9696b631854d
PeHaa <info@pehaa.com>
2020-09-15 08:30:56 +0200

    add container class

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   return (
     <>
       <Header />
-      <div className="container">
+      <div className="container min-vh-100">
         <div className="row my-5">
           <div className="col-lg-3">
             <div style={{ position: "sticky", top: 0 }}>
